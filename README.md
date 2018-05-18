eb2430c7478507bc13b6a05d16dc3a73e036e2ed
Mabweh Isaac <isaac@movingwalls.com>
2018-05-19 02:48:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ghflutter
+
+A new Flutter application as learned from https:&#x2F;&#x2F;www.raywenderlich.com&#x2F;188257&#x2F;getting-started-with-flutter.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/ghflutter.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'member.dart';
+import 'strings.dart';
+
+var _members = <Member>[];
+final _biggerFont = const TextStyle(fontSize: 18.0);
+
+class GHFlutter extends StatefulWidget {
+  @override
+  createState() => GHFlutterState();
+}
+
+class GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text(Strings.appTitle),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: _members.length * 2,
+          itemBuilder: (BuildContext context, int position) {
+            if (position.isOdd) return Divider();
+
+            final index = position ~/ 2;
+
+            return _buildRow(index);
+          }),
+    );
+  }
+
+  void initState() {
+    super.initState();
+
+    _loadData();
+  }
+
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      final membersJSON = json.decode(response.body);
+
+      for (var memberJSON in membersJSON) {
+        final member = Member(memberJSON["login"], memberJSON["avatar_url"]);
+        _members.add(member);
+      }
+    });
+  }
+
+  Widget _buildRow(int i) {
+    return Card(
+        color: Colors.red[50],
+        child: ListTile(
+          title: Text("${_members[i].login}", style: _biggerFont),
+          leading: CircleAvatar(
+              backgroundColor: Colors.green,
+              backgroundImage: NetworkImage(_members[i].avatarUrl)
+          ),
+        )
+    );
+  }
+}
